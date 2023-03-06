fc97405c346745ed525f439d9a35d27f78c854bc
Jared Whiklo <jwhiklo@gmail.com>
2023-03-06 12:21:36 -0600

    Missed migration population functions
    Use DJANGO_SETTINGS_MODULE variable in docker scripts

--- bagitobjecttransfer/docker/django-rq/django-rq-entrypoint.sh
+++ bagitobjecttransfer/docker/django-rq/django-rq-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Creating migrations..."
-python3 manage.py makemigrations --no-input --settings=bagitobjecttransfer.settings.docker
+python3 manage.py makemigrations --no-input --settings=${DJANGO_SETTINGS_MODULE}
 
 echo "Applying migrations..."
-python3 manage.py migrate --no-input --settings=bagitobjecttransfer.settings.docker
+python3 manage.py migrate --no-input --settings=${DJANGO_SETTINGS_MODULE}
 
 echo "Running Django RQ worker"
-python3 manage.py rqworker default --settings=bagitobjecttransfer.settings.docker
+python3 manage.py rqworker default --settings=${DJANGO_SETTINGS_MODULE}
--- bagitobjecttransfer/docker/django/django-entrypoint.sh
+++ bagitobjecttransfer/docker/django/django-entrypoint.sh
@@ -4,7 +4,7 @@ echo "Creating job directory"
 mkdir -p /app/media/jobs
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --clear --no-input -v0 --settings=bagitobjecttransfer.settings.docker
+python3 manage.py collectstatic --clear --no-input -v0 --settings=${DJANGO_SETTINGS_MODULE}
 
 echo "Running Django app"
-python3 manage.py runserver 0.0.0.0:8000 --settings=bagitobjecttransfer.settings.docker
+python3 manage.py runserver 0.0.0.0:8000 --settings=${DJANGO_SETTINGS_MODULE}
