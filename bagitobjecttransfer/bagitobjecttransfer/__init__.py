f082cffdfb08af9cc56e6ffaa0a9c96068e0390c
Jared Whiklo <jwhiklo@gmail.com>
2023-06-14 16:55:32 -0500

    Fake out PyMySQL version

--- bagitobjecttransfer/bagitobjecttransfer/__init__.py
+++ bagitobjecttransfer/bagitobjecttransfer/__init__.py
@@ -1,3 +1,6 @@
 import pymysql
 
+# Fake PyMySQL's version and install as MySQLdb
+# https://adamj.eu/tech/2020/02/04/how-to-use-pymysql-with-django/
+pymysql.version_info = (1, 4, 3, "final", 0)
 pymysql.install_as_MySQLdb()
